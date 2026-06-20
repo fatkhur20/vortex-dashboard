3ca9286adad23deb8ec3084f809b43b664e8688b
fatkhur20 <litepubgm46@gmail.com>
2026-06-21 01:07:08 +0700

    Use external script for manifest patching

---
+++ .github/workflows/patch_manifest.py
@@ -0,0 +1,11 @@
+import sys
+path = sys.argv[1]
+m = open(path).read()
+perms = '''    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+    <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
+    <uses-permission android:name="android.permission.ACCESS_BACKGROUND_LOCATION"/>
+'''
+m = m.replace('<application', perms + '<application')
+m = m.replace('android:label="vortex_dashboard"', 'android:label="Vortex Dashboard"')
+open(path, 'w').write(m)
+print('Patched:', path)
