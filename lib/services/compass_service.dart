376699b6e2a3b99f35881c1cca74f4ad4f8f9ed6
fatkhur20 <litepubgm46@gmail.com>
2026-06-20 14:24:28 +0700

    Remove flutter_compass, use sensors_plus tilt-compensated compass

--- lib/services/compass_service.dart
+++ lib/services/compass_service.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:math';
-import 'package:flutter_compass/flutter_compass.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
 class CompassService {
@@ -17,41 +16,60 @@ class CompassService {
   bool _isCalibrated = false;
   bool get isCalibrated => _isCalibrated;
 
-  StreamSubscription? _compassSubscription;
   StreamSubscription? _magnetometerSubscription;
+  StreamSubscription? _accelerometerSubscription;
+
+  List<double> _gravity = [0, 0, 0];
+  List<double> _geomagnetic = [0, 0, 0];
 
   Future<void> startListening() async {
-    _compassSubscription = FlutterCompass.events?.listen(
-      (event) {
-        if (event.heading != null) {
-          _currentHeading = event.heading!;
-          _isCalibrated = event.accuracy != null && event.accuracy! > 0;
-          _headingController.add(_currentHeading);
-        }
+    _accelerometerSubscription = accelerometerEventStream().listen(
+      (AccelerometerEvent event) {
+        _gravity = [event.x, event.y, event.z];
+        _computeHeading();
       },
-      onError: (error) {
-        _startMagnetometerFallback();
+    );
+
+    _magnetometerSubscription = magnetometerEventStream().listen(
+      (MagnetometerEvent event) {
+        _geomagnetic = [event.x, event.y, event.z];
+        _computeHeading();
       },
     );
   }
 
-  void _startMagnetometerFallback() {
-    _magnetometerSubscription = magnetometerEventStream().listen(
-      (MagnetometerEvent event) {
-        final radians = atan2(event.y, event.x);
-        var degrees = radians * 180 / pi;
-        degrees = (degrees + 360) % 360;
-        _currentHeading = degrees;
-        _headingController.add(_currentHeading);
-      },
-    );
+  void _computeHeading() {
+    final gx = _gravity[0], gy = _gravity[1], gz = _gravity[2];
+    final mx = _geomagnetic[0], my = _geomagnetic[1], mz = _geomagnetic[2];
+
+    final norm = sqrt(gx * gx + gy * gy + gz * gz);
+    if (norm == 0) return;
+    final ax = gx / norm, ay = gy / norm, az = gz / norm;
+
+    final ex = my * az - mz * ay;
+    final ey = mz * ax - mx * az;
+    final ez = mx * ay - my * ax;
+
+    final eNorm = sqrt(ex * ex + ey * ey + ez * ez);
+    if (eNorm == 0) return;
+    final nx = ex / eNorm, ny = ey / eNorm, nz = ez / eNorm;
+
+    final mx2 = mx * nx + my * ny + mz * nz;
+    final my2 = mx * (ay * nz - az * ny) + my * (az * nx - ax * nz) + mz * (ax * ny - ay * nx);
+
+    var heading = atan2(my2, mx2) * 180 / pi;
+    heading = (heading + 360) % 360;
+
+    _currentHeading = heading;
+    _isCalibrated = true;
+    _headingController.add(_currentHeading);
   }
 
   void stopListening() {
-    _compassSubscription?.cancel();
-    _compassSubscription = null;
     _magnetometerSubscription?.cancel();
     _magnetometerSubscription = null;
+    _accelerometerSubscription?.cancel();
+    _accelerometerSubscription = null;
   }
 
   String get headingString {
